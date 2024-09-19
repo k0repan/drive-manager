aa51301f75c6c21705e5ddfd1e16232ad81e4519
k0repan <i.m.korepan@gmail.com>
2024-09-20 00:25:51 +0300

    A Hello World version

---
+++ README.md
@@ -0,0 +1,3 @@
+# A GoogleDrive Manager Telegram bot.
+
+That's just a "Hello World" version :D
--- main.py
+++ main.py
@@ -13,20 +13,22 @@ DOCUMENT_ID = "195j9eDD3ccgjQRttHhJPymLJUCOUjs-jmwTrekvdjFE"
 
 def main():
   creds = None
+  token_path = ".config/token.json"
+  credentials_path = ".config/credentials.json"
 
-  if os.path.exists("token.json"):
-    creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+  if os.path.exists(token_path):
+    creds = Credentials.from_authorized_user_file(token_path, SCOPES)
 
   if not creds or not creds.valid:
     if creds and creds.expired and creds.refresh_token:
       creds.refresh(Request())
     else:
       flow = InstalledAppFlow.from_client_secrets_file(
-          ".config/credentials.json", SCOPES
+          credentials_path, SCOPES
       )
       creds = flow.run_local_server(port=0)
 
-    with open("token.json", "w") as token:
+    with open(token_path, "w") as token:
       token.write(creds.to_json())
 
   try:
